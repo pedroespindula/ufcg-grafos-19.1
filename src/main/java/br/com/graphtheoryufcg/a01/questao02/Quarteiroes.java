8781f5ce187a3c5ddaee9d72fcd781c0985e0e71
Pedro Espíndula <joao.espindula@ccc.ufcg.edu.br>
2019-04-14 20:46:05 -0300

    Add A01Q02 - Quarteiroes initial resolution

--- src/main/java/br/com/graphtheoryufcg/a01/questao02/Quarteiroes.java
+++ src/main/java/br/com/graphtheoryufcg/a01/questao02/Quarteiroes.java
@@ -1,9 +1,18 @@
 package br.com.graphtheoryufcg.a01.questao02;
 
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+
+import static br.com.graphtheoryufcg.JGraphUtil.*;
+
 public class Quarteiroes {
 
     public static void main(String[] args) {
-        System.out.println("Hello Quarteiroes!");
+        final String PATH_TO_CSV = "./input/q02.csv";
+
+        Graph<String, DefaultEdge> graph = importGraphFromCSVMatrix(PATH_TO_CSV);
+
+        printGraph(graph);
     }
 
 }
