a644138ae159fdc128b6c28418334ad03825f1d4
Pedro Espíndula <joao.espindula@ccc.ufcg.edu.br>
2019-04-14 20:45:38 -0300

    Add A01Q01 - Astronautas resolution

--- src/main/java/br/com/graphtheoryufcg/a01/questao01/Astronautas.java
+++ src/main/java/br/com/graphtheoryufcg/a01/questao01/Astronautas.java
@@ -1,9 +1,20 @@
 package br.com.graphtheoryufcg.a01.questao01;
 
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+
+import static br.com.graphtheoryufcg.JGraphUtil.*;
+
 public class Astronautas {
 
     public static void main(String[] args) {
-        System.out.println("Hello Astronautas!");
+        final String PATH_TO_CSV = "./input/q01.csv";
+
+        Graph<String, DefaultEdge> graph = importGraphFromCSVMatrix(PATH_TO_CSV);
+        Graph<String, DefaultEdge> compGraph = getCompGraph(graph);
+
+        printEdgesFromGraph(compGraph);
     }
+
 
 }
