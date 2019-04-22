eac672ff09ac1d7a2cb940c6cbcccaad4e5c2d75
Rob <bruno.robsiq@gmail.com>
2019-04-22 19:03:00 -0300

    Finish Q02

--- src/main/java/br/com/graphtheoryufcg/a01/questao02/Quarteiroes.java
+++ src/main/java/br/com/graphtheoryufcg/a01/questao02/Quarteiroes.java
@@ -2,10 +2,11 @@ package br.com.graphtheoryufcg.a01.questao02;
 
 import org.jgrapht.Graph;
 import org.jgrapht.GraphMapping;
-import org.jgrapht.alg.isomorphism.VF2GraphIsomorphismInspector;
+import org.jgrapht.alg.isomorphism.VF2SubgraphIsomorphismInspector;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
-import java.util.Iterator;
+
+import java.util.*;
 
 import static br.com.graphtheoryufcg.JGraphUtil.*;
 
@@ -16,32 +17,31 @@ public class Quarteiroes {
 
         Graph<String, DefaultEdge> graph = importGraphFromCSVMatrix(PATH_TO_CSV);
 
-        Graph<String, DefaultEdge> result = findCityBlock(graph);
-        printGraph(result);
+        Set<Set<String>> result = findCityBlocks(graph);
+
+        System.out.println(result.toString());
     }
 
-    public static Graph<String, DefaultEdge> findCityBlock(Graph<String, DefaultEdge> in) {
+    private static Set<Set<String>> findCityBlocks(Graph<String, DefaultEdge> in) {
         Graph<String, DefaultEdge> triangleGraph = createTriangleGraph();
 
-        Graph<String, DefaultEdge> letras = new SimpleGraph<>(DefaultEdge.class);
-
-        triangleGraph.addVertex("1");
-        triangleGraph.addVertex("2");
-        triangleGraph.addVertex("3");
-
-        triangleGraph.addEdge("a", "b");
-        triangleGraph.addEdge("a", "c");
-        triangleGraph.addEdge("c", "b");
-
-        VF2GraphIsomorphismInspector<String, DefaultEdge> isomorphismInspector = new VF2GraphIsomorphismInspector<>(letras, triangleGraph);
+        VF2SubgraphIsomorphismInspector<String, DefaultEdge> isomorphismInspector =
+                new VF2SubgraphIsomorphismInspector<>(in, triangleGraph);
 
         Iterator<GraphMapping<String, DefaultEdge>> iterator = isomorphismInspector.getMappings();
 
+        Set<Set<String>> blocks = new HashSet<Set<String>>();
+
         while (iterator.hasNext()) {
-            System.out.println(iterator.next() + " !");
+            GraphMapping<String, DefaultEdge> mapping = iterator.next();
+            Set<String> block = new HashSet<>();
+            block.add(mapping.getVertexCorrespondence("1", false));
+            block.add(mapping.getVertexCorrespondence("2", false));
+            block.add(mapping.getVertexCorrespondence("3", false));
+            blocks.add(block);
         }
 
-        return in;
+        return blocks;
     }
 
     private static Graph<String, DefaultEdge> createTriangleGraph() {
@@ -55,7 +55,6 @@ public class Quarteiroes {
         triangleGraph.addEdge("1", "3");
         triangleGraph.addEdge("3", "2");
 
-        printGraph(triangleGraph);
         return triangleGraph;
     }
 }
